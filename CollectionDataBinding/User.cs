c7c210490ff6f8d92398afb5ef7aeee082a9e5bb
manuel jesus queb coba <19470360@itcampeche.edu.mx>
2020-11-05 20:09:24 -0600

    Versión inicial, utilizando sólo manejo de eventos. No hace uso de data binding

---
+++ CollectionDataBinding/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeNotificationSample
+{
+    class User
+    {
+        private string name;
+        public string Name 
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                }
+            }
+        }
+
+    }
+}
